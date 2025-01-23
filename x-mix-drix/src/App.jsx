beacbe597a0f19785015a6987ba36f4402145762
AvivTad <avivitv@gmail.com>
2025-01-23 23:29:32 +0200

    Design updates

--- x-mix-drix/src/App.jsx
+++ x-mix-drix/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState("X");
-  const [winner, setWinner] = useState(null); 
+  const [winner, setWinner] = useState(null);
 
   const handleCellClick = (index) => {
     if (winner || board[index]) return;
@@ -13,9 +13,9 @@ function App() {
     newBoard[index] = currentPlayer;
     setBoard(newBoard);
 
-    const gameResult = checkWinner(newBoard);
-    if (gameResult) {
-      setWinner(gameResult);
+    const result = checkWinner(newBoard);
+    if (result) {
+      setWinner(result);
     } else if (!newBoard.includes(null)) {
       setWinner("tie");
     } else {
@@ -23,25 +23,23 @@ function App() {
     }
   };
 
-  const checkWinner = (board) => {
+  const checkWinner = (boardState) => {
     const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6], 
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],  
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],  
+      [0, 4, 8], [2, 4, 6]
     ];
-
     for (let combo of winningCombinations) {
       const [a, b, c] = combo;
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return board[a]; 
+      if (
+        boardState[a] &&
+        boardState[a] === boardState[b] &&
+        boardState[a] === boardState[c]
+      ) {
+        return boardState[a];
       }
     }
-    return null; 
+    return null;
   };
 
   const resetGame = () => {
@@ -53,9 +51,14 @@ function App() {
   return (
     <div className="App">
       <h1>X Mix Drix</h1>
+
       <div className="board">
         {board.map((value, index) => (
-          <div className="cell" key={index} onClick={() => handleCellClick(index)}>
+          <div
+            className="cell"
+            key={index}
+            onClick={() => handleCellClick(index)}
+          >
             {value && (
               <img
                 src={
@@ -69,14 +72,13 @@ function App() {
           </div>
         ))}
       </div>
-      
-      {/* tie*/}
+
       {winner && (
         <div className="winner-message">
           {winner === "tie" ? (
-            <p>It is a Tie!</p>
+            <p>It&apos;s a Tie!</p>
           ) : (
-            <p>The Winner is {winner}!</p>
+            <p className="winner-animate">The Winner is {winner}!</p>
           )}
           <button onClick={resetGame}>Play Again</button>
         </div>
